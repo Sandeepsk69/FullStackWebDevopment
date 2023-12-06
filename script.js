9f26fd5d91876309228e96df04f9c0a4f5324f8e
Sandeepsk69 <sandeep.kumar@msds.christuniversity.in>
2023-12-06 11:39:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var a=10;
+letb=5;
+var c;
+c=a+b;
+console.log(c);
+
+function add(params) {
+    var a = document.getElementById(number1).value;
+    var b = document.getElementById(number2).value;
+
+    var c = parseInt(a)+parseInt(b);
+    document.getElementById(number2).value;
+    
+
+}
